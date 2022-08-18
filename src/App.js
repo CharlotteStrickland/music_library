4dd8d79bb2ce8bfc8cff4a5288f202ce4efcffc5
Charlotte Strickland <charlottemarie81@gmail.com>
2022-08-17 20:11:42 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+import Gallery from './components/Gallery'
+import SearchBar from './components/SearchBar'
+import './App.css';
+
+function App(){
+    let [search, setSearch] = useState('')
+    let [message, setMessage] = useState('Search for Music!')
+    let [data, setData] = useState([])
+
+    //const API_URL = 'https://itunes.apple.com/search?term='
+
+    useEffect(() => {
+        if(search) {
+        const fetchData = async () => {
+            document.title = `${search} Music`
+            const response = await fetch('API_URL = search')
+            const resData = await response.json();
+            console.log(resData)
+            if (resData.results.length > 0) {
+                setData(resData.results);
+                setMessage('');
+            } else {
+                setMessage('No Songs Found');
+            }
+        }
+        fetchData();
+    }
+}, [search]);
+
+  const handleSearch = (e, term) => {
+      e.preventDefault()
+      setSearch(term)
+    }
+    
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Music Library</h1>
+            </header>
+            <div className="content">
+            <SearchBar handleSearch = {handleSearch} />
+            {message ? <h2>{message}</h2> : null}
+            <Gallery data={data} />
+        </div>
+        </div>
+    );
+}    
+
+export default App
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+const SearchBar = (props) => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    return (
+        <div className="search-bar">
+        <form onSubmit={(e)=> props.handleSearch(e, searchTerm)}>
+            <input type="text" placeholder="Search for music..." onChange={(e)=>setSearchTerm(e.target.value)} />  {/*props.handleSearch(e, e.target.value) */}
+            <button type="submit">Search</button>
+        </form>
+        {/* {message ? <h2>{message}</h2> : null} */}
+        <form onSubmit={(e)=>setSearchTerm('')}><button>Clear</button></form>
+        </div>
+    );
+};
+
+export default SearchBar;  

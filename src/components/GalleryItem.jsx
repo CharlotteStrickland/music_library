d5530625920c73fc2021da2e94e99cfb38433cc2
Charlotte Strickland <charlottemarie81@gmail.com>
2022-08-19 16:50:58 -0400

    router add

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -1,7 +1,26 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom'
 
 function GalleryItem(props){
-    let [view, setView] = useState(true);
+    let [view, setView] = useState(false);
+
+    const simpleStyle = {
+        'width': '25vw',
+        'height': '20vh',
+        'border': '1px solid black',
+        'margin': '2px'
+    }
+    
+    const detailStyle = {
+        'width': '80vw',
+        'height': '20vh',
+        'border': '1px solid black',
+        'margin': '2px',
+        'backgroundImage': `url(${props.item.artworkUrl100})`,
+        'backgroundRepeat': 'no-repeat',
+        'backgroundSize': 'cover',
+        'color': 'yellow'
+    }
 
     const simpleView = () => {
         return (
@@ -12,21 +31,29 @@ function GalleryItem(props){
         );
     };
     
-    const detailedStyle = {'backgroundImage':`url(${props.data.artworkUrl100})`};
-    // let date = props.data.releaseDate.getDate()
-    const detailedView = () => {
+    const detailView = () => {
         return (
-            <div className="gallery-item-detailed" style={detailedStyle}>
-                <h3>{props.data.trackName}</h3>
-                <h4>{props.data.collectionName}</h4>
-                <h5>{props.data.primaryGenreName}</h5>
-                <h5>{props.data.releaseDate}</h5>
+            <div style={detailStyle}>
+                <h2>{props.item.trackName}</h2>
+                <h3>
+                    <Link to={`/artist/${props.item.artistId}`}>
+                        {props.item.artistName}
+                    </Link>
+                </h3>
+                <h3>
+                    <Link to={`/album/${props.item.collectionId}`}>
+                        {props.item.collectionName}
+                    </Link>
+                </h3>
+                <h4>{props.item.primaryGenreName}</h4>
+                <h4>{props.item.releaseDate}</h4>
             </div>
-        );
-    };
+        )
+    }
+
     return (
         <div onClick={() => setView(!view)} style={{'display': 'inline-block'}}>
-            {view ? detailedView() : simpleView()}
+            {view ? detailView() : simpleView()}
         </div>
     );
 };
